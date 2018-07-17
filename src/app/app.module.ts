c0efc1eaedbaa39f920462d96ec6a75d8bc0762f
Kalter <kalterxad34@gmail.com>
2018-07-17 18:07:40 +0300

    about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { MainModule } from './main/main.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutComponent],
   imports: [BrowserModule, SharedModule, MainModule, ],
   providers: [],
   bootstrap: [AppComponent]
