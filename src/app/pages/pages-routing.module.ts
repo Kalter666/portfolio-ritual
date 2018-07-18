b57aed8a0a87c5ca8d1572714c3137b087bb775d
Kalter <kalterxad34@gmail.com>
2018-07-18 19:01:20 +0300

    service page

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PricesComponent } from './prices/prices.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
-  { path: 'prices', component: PricesComponent }
+  { path: 'prices', component: PricesComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'shop', redirectTo: 'contacts'},
+  { path: '**', redirectTo: '../'}
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PricesComponent } from './prices/prices.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PagesRoutingModule
   ],
-  declarations: [PricesComponent]
+  declarations: [PricesComponent, ContactsComponent, ServicesComponent]
 })
 export class PagesModule { }
