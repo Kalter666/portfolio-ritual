bbe46b635d947a94431efd0fd5edd8b936846f9c
Kalter <kalterxad34@gmail.com>
2018-07-19 14:49:46 +0300

    articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 
-const appRoutes: Routes = [
+export const appRoutes: Routes = [
   { path: '', component: MainComponent },
   { path: 'about', component: AboutComponent },
   { path: 'page', loadChildren: './pages/pages.module#PagesModule' },
